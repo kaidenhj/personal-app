06dcb3aa8801848130b2df7347db1dd00bc5db35
Kaiden Jones <kaidenhj@icloud.com>
2025-03-20 16:10:43 -0400

    Made screen darker and slightly adjusted text.

--- src/components/HeroSection/heroElements.js
+++ src/components/HeroSection/heroElements.js
@@ -12,6 +12,17 @@ export const HeroContainer = styled.div`
     position: relative;
     z-index: 1;
 
+    :before {
+        conetent: '';
+        position: absolute;
+        top: 0;
+        left: 0;
+        right: 0;
+        bottom: 0;
+        background: linear-gradiant(180deg, rgba(0,0,0,0.2) 0%, rgba(0,0,0,0.6) 100%), 
+        linear-gradiant(180deg, rgba(0,0,0,0.2) 0%, transparent 100%);
+        z-index: 2;
+    }
 `
  
 export const HeroBg = styled.div`
--- src/components/navbar/navBarElement.js
+++ src/components/navbar/navBarElement.js
@@ -4,6 +4,7 @@ import { Link as LinkRouter } from 'react-router-dom';
 export const Nav = styled.nav`
     background: #000;
     height: 80px;
+    margin-top: -80px;
     display: flex;
     justify-content: center;
     align-items: center;
