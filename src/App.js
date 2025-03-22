d5afbf3a9566b6c98735f086a661fd95a81e08a1
Kaiden Jones <kaidenhj@icloud.com>
2025-03-22 16:25:55 -0400

    Ex Route button in side bar takes user to routing page

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@
 import './App.css';
 import Home from './pages';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Thing from './pages/thing';
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/" element={<Home />} exact />
+        <Route path="/thing" element={<Thing />} exact />
       </Routes>
     </Router>
   );
---
+++ src/pages/thing.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Thing = () => {
+  return (
+    <div>
+      <h1>Routing Page</h1>
+    </div>
+  );
+}
+
+
+export default Thing;
